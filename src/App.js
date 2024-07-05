bf2359baec8ecb0e3afd0df12f55326ee0bf7d21
Jakub Madejczyk <jbmk01@gmail.com>
2024-07-05 15:48:21 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import logo from "./logo.svg";
+import "./App.css";
+import particlesOptions from "./particles.json";
+
+function App() {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    if (init) {
+      return;
+    }
+
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {init && <Particles options={particlesOptions} />}
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
